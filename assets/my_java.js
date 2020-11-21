9c521ef7ab7605f471442bb6591be615b94667be
madatoo <magda.ruszaj@gmail.com>
2020-11-21 21:09:12 +0000

    almost final commit

--- assets/my_java.js
+++ assets/my_java.js
@@ -6,3 +6,16 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+$(function() {
+var selectedClass = "";
+$(".filter").click(function(){
+selectedClass = $(this).attr("data-rel");
+$("#gallery").fadeTo(100, 0.1);
+$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+setTimeout(function() {
+$("."+selectedClass).fadeIn().addClass('animation');
+$("#gallery").fadeTo(300, 1);
+}, 300);
+});
+});
